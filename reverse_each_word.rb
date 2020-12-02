76609fcfbc3221e16be0ae93b2d9bb8cbfa9c816
darien bryant <darienbryant11@gmail.com>
2020-12-02 23:15:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,12 +4,11 @@ def reverse_each_word (string)
  new_arr = string.split (" ") 
 
   reversed_arr = []
-  new_arr.each  do |element|
-    reversed_arr << element.reverse 
+  new_arr.each   {|element| reversed_arr << element.reverse} 
     
  
     
-  end 
+  
   
   reversed_arr.join(" ") 
   
