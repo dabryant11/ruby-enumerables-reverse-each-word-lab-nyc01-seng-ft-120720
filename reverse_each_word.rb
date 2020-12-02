c6683b46924d10479807ee0999a11620009f2f60
darien bryant <darienbryant11@gmail.com>
2020-12-02 23:25:11 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word (string)
+  
+  
+ new_arr = string.split (" ") 
+
+  
+  reversed_element_arr =  new_arr.collect {|element| element.reverse} 
+  
+  
+  reversed_element_arr.join(" ")
+  
+end
+
+
+
